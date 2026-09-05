5999bf595d8513df8e18ae358200a461d68af484
Erna Hidayanti Nur falah_2225250036_3B <ernahdynti1205@gmail.com>
2026-09-05 11:52:44 +0700

    Add files via upload

---
+++ Pertemuan-01/keliling_lingkaran.py
@@ -0,0 +1,12 @@
+print("=== PROGRAM HITUNG KELILING LINGKARAN ===")
+
+# 1. Menerima Input dari Pengguna
+jari_jari = float(input("Masukkan jari-jari lingkaran (cm): "))
+
+# 2. Proses Perhitungan (Sesuai Identifikasi IPO)
+keliling = 2 * 3.14 * jari_jari
+
+
+# 3. Menampilkan Output
+print("\n--- HASIL PERHITUNGAN ---")
+print(f"Keliling lingkaran: {keliling} cm")
